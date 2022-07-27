b127a1a9da65e23b170d7f898d74503b39081a91
PrasoonTiwari23 <ptiffco@gmail.com>
2022-07-28 01:46:48 +0530

    First Commit

---
+++ app.js
@@ -0,0 +1,43 @@
+//importing modeules
+var express = require("express");
+var mongoose = require("mongoose");
+var bodyparser = require("body-parser");
+var cors = require("cors");
+var path = require("path");
+var app = express();
+const route = require("./routes/route");
+
+//connection to mongodb
+mongoose.connect("mongodb://localhost:27017/contactlist");
+//on connection
+mongoose.connection.on("connected", () => {
+	console.log("Connection to mongodb is successfull");
+});
+mongoose.connection.on("error", (err) => {
+	if (err) {
+		console.log("Error in connecting to moongodb : " + err);
+	}
+});
+
+//port no.
+const port = 3000;
+
+//adding middle ware- cors
+app.use(cors());
+//adding body-parser
+app.use(bodyparser.json());
+
+//static files
+app.use(express.static(path.join(__dirname, "public")));
+//routes
+
+app.use("/api", route);
+//testing server
+
+app.get("/", (req, res) => {
+	res.send("Helloo to port 3000");
+});
+
+app.listen(port, () => {
+	console.log("Server started at port " + port);
+});
